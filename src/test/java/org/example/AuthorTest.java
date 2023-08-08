cc6e7af92b22da43bce44d2b34bb27783d565f10
nikolailic-competec <nikola.ilic@competec.ch>
2023-08-08 08:24:26 +0200

    adjusted tests

--- src/test/java/org/example/AuthorTest.java
+++ src/test/java/org/example/AuthorTest.java
@@ -5,35 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AuthorTest
 {
 
-    Author author = mock(Author.class);
+    Author author;
 
-    @Test
-    void testValidAuthor()
+    @BeforeEach
+    void setUp()
     {
 
-        when(author.getFirstName()).thenReturn("Big");
-        when(author.getLastName()).thenReturn("Fish");
-        when(author.getEmail()).thenReturn("big.fish@gmail.com");
-
-        assertEquals("Big", author.getFirstName());
-        assertEquals("Fish", author.getLastName());
-        assertEquals("big.fish@gmail.com", author.getEmail());
+        author = new Author("Big", "Fish", "big.fish@gmail.com");
     }
 
     @Test
     void testInvalidEmail()
     {
-        when(author.getFirstName()).thenReturn("Big");
-        when(author.getLastName()).thenReturn("Fish");
-        when(author.getEmail()).thenReturn("big.fish_gmail.com");
 
         assertThrows(IllegalArgumentException.class, () ->
-            new Author(author.getFirstName(), author.getLastName(), author.getEmail())
+            new Author(author.getFirstName(), author.getLastName(), "test_gmail.com")
         );
     }
 }
--- src/test/java/org/example/BookTest.java
+++ src/test/java/org/example/BookTest.java
@@ -6,54 +6,35 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BookTest
 {
 
     Author author = mock(Author.class);
-    Book book = mock(Book.class);
+    Book book;
 
-    @Test
-    void validBook()
+    @BeforeEach
+    void setUp()
     {
 
         LocalDate localDate = LocalDate.of(2023, 8, 1);
-
-        when(book.getAuthor()).thenReturn(author);
-        when(book.getPublication()).thenReturn(localDate);
-        when(book.getPages()).thenReturn(300);
-        when(book.getTitle()).thenReturn("test");
-        when(book.getGenre()).thenReturn("Fiction");
-
-        assertEquals("test", book.getTitle());
-        assertEquals(author, book.getAuthor());
-        assertEquals(localDate, book.getPublication());
-        assertEquals(300, book.getPages());
-        assertEquals("Fiction", book.getGenre());
+        book = new Book("test", author, localDate, 300, "Fiction");
     }
 
     @Test
     void invalidPages()
     {
-        LocalDate localDate = LocalDate.of(2023, 8, 1);
 
-        when(book.getAuthor()).thenReturn(author);
-        when(book.getPublication()).thenReturn(localDate);
-        when(book.getPages()).thenReturn(-1);
-        when(book.getTitle()).thenReturn("test");
-        when(book.getGenre()).thenReturn("Fiction");
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Book(book.getTitle(), author, book.getPublication(), book.getPages(), book.getGenre());
-        });
+        assertThrows(IllegalArgumentException.class, () ->
+            new Book(book.getTitle(), author, book.getPublication(), -1, book.getGenre()));
     }
 
     @Test
     void get_authorFirstname_from_book()
     {
 
-        when(book.getAuthor()).thenReturn(author);
         when(author.getFirstName()).thenReturn("Big");
 
         assertEquals("Big", book.getAuthor().getFirstName());
